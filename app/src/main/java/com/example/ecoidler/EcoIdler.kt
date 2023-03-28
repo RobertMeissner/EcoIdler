52faa3dd6cf911c04c88899f43351d1c10b30a7f
RMeissnerCC <robert.meissner@codecentric.de>
2023-03-28 17:01:42 +0200

    Adding rough top bar

--- app/src/main/java/com/example/ecoidler/EcoIdler.kt
+++ app/src/main/java/com/example/ecoidler/EcoIdler.kt
@@ -1,33 +1,80 @@
 package com.example.ecoidler
 
 import android.content.res.Configuration
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.ecoidler.ui.theme.EcoIdlerTheme
 
 @Composable
 fun EcoIdler() {
+
+    val navController = rememberNavController()
+    val scaffoldState = rememberScaffoldState()
+
     Surface(
         modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
     ) {
-        Column {
+        Scaffold(
+            scaffoldState = scaffoldState,
+            topBar = { TopAppBarCompose() },
+//            bottomBar = { BottomBar(navController)            }
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = "home",
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable("home") {
+                    Column {
 
-            Greeting("EcoIdler")
-            val materials = listOf<MaterialStats>(MaterialStats(name = "wood", amount = 10))
-            Stats(stats = materials)
-            MaterialCounter(material_name = "Wood", onClick = { })
-            MaterialCounter(material_name = "Stone", onClick = { })
+                        Greeting("EcoIdler")
+                        val materials =
+                            listOf<MaterialStats>(MaterialStats(name = "wood", amount = 10))
+                        Stats(stats = materials)
+                        MaterialCounter(material_name = "Wood", onClick = { })
+                        MaterialCounter(material_name = "Stone", onClick = { })
+                    }
+                }
+            }
         }
     }
+
+
+}
+
+
+@Composable
+fun TopAppBarCompose() {
+    val context = LocalContext.current
+    val showMenu = remember {
+        mutableStateOf(false)
+    }
+    TopAppBar(
+        contentColor = MaterialTheme.colors.primaryVariant,
+        title = { Text(text = "Top app bar") },
+        navigationIcon = {
+            IconButton(onClick = {
+                Toast.makeText(context, "TopAppBar menu clicked", Toast.LENGTH_SHORT).show()
+            }) {
+                Icon(Icons.Default.Menu, contentDescription = null)
+            }
+        })
 }
 
 @Composable
